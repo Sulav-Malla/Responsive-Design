64fab1c315fb910c12709ecfdacd7100959be4e1
uniquemozilla22 <uniqgaming26@gmail.com>
2024-12-23 09:53:25 -0500

    Task flexbox

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,28 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="container">
+        <div className="navigation_bar">
+          <h1>Logo</h1>
+          <div>
+            <buttons>User</buttons>
+            <buttons>Cart</buttons>
+          </div>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+    </>
+  );
 }
 
 export default App;
